57a940fc2ab0f7e66323bca3dc84995f031040ce
SanjanaBoggaram <138201692+SanjanaBoggaram@users.noreply.github.com>
2025-03-19 10:11:49 +0530

    Update hello.cpp

--- CC_TA/main/hello.cpp
+++ CC_TA/main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, PES1UG22CS524\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I am in I section \n";
   return 0;
 }
